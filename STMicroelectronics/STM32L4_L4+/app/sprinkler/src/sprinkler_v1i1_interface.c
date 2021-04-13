efb9449b028480b0e1c8a55baa0082cd7300d5d6
Marcos Perez Mokarzel <mamokarz@microsoft.com>
2021-04-12 17:24:23 -0700

    Add query to IPC.

--- STMicroelectronics/STM32L4_L4+/app/sprinkler/src/sprinkler_v1i1_interface.c
+++ STMicroelectronics/STM32L4_L4+/app/sprinkler/src/sprinkler_v1i1_interface.c
@@ -23,8 +23,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-static az_result sprinkler_1_call_w_str(az_span name, az_span model_in, az_span* model_out);
-
 /*
  * Concrete implementations of the sprinkler 1 water_now commands.
  */
@@ -101,47 +99,19 @@ static az_result sprinkler_1_stop_span_wrapper(az_span model_in_span, az_span* m
 static const az_ulib_capability_descriptor SPRINKLER_1_CAPABILITIES[SPRINKLER_1_CAPABILITY_SIZE] = {
   AZ_ULIB_DESCRIPTOR_ADD_COMMAND(
       SPRINKLER_1_WATER_NOW_COMMAND_NAME,
-      sprinkler_1_water_now_concrete),
-  AZ_ULIB_DESCRIPTOR_ADD_COMMAND(SPRINKLER_1_STOP_COMMAND_NAME, sprinkler_1_stop_concrete)
+      sprinkler_1_water_now_concrete,
+      sprinkler_1_water_now_span_wrapper),
+  AZ_ULIB_DESCRIPTOR_ADD_COMMAND(
+      SPRINKLER_1_STOP_COMMAND_NAME, 
+      sprinkler_1_stop_concrete, 
+      sprinkler_1_stop_span_wrapper)
 };
 
 static const az_ulib_interface_descriptor SPRINKLER_1_DESCRIPTOR = AZ_ULIB_DESCRIPTOR_CREATE(
     SPRINKLER_1_INTERFACE_NAME,
     SPRINKLER_1_INTERFACE_VERSION,
     SPRINKLER_1_CAPABILITY_SIZE,
-    sprinkler_1_call_w_str,
     SPRINKLER_1_CAPABILITIES);
-
-static az_result sprinkler_1_call_w_str(az_span name, az_span model_in_span, az_span* model_out_span)
-{
-  uint8_t index;
-  az_result result;
-
-  for (index = 0; index < SPRINKLER_1_DESCRIPTOR.size; index++)
-  {
-    if (az_span_is_content_equal(name, SPRINKLER_1_DESCRIPTOR.capability_list[index].name))
-    {
-      break;
-    }
-  }
-
-  switch (index)
-  {
-    case SPRINKLER_1_WATER_NOW_COMMAND:
-      result = sprinkler_1_water_now_span_wrapper(model_in_span, model_out_span);
-      break;
-    case SPRINKLER_1_STOP_COMMAND:
-    {
-      result = sprinkler_1_stop_span_wrapper(model_in_span, model_out_span);
-      break;
-    }
-    default:
-      result = AZ_ERROR_ITEM_NOT_FOUND;
-      break;
-  }
-
-  return result;
-}
 
 az_result sprinkler_v1i1_interface_publish(void)
 {
