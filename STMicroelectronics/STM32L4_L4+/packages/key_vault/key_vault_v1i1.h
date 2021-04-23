6fb43d91086efba2ea6448bb2cb4c92bb3cf737d
Marcos Perez Mokarzel <mamokarz@microsoft.com>
2021-04-23 16:42:19 -0700

    Create key_vault package for Device Manager integrated with ThreadX Modules.

--- STMicroelectronics/STM32L4_L4+/packages/key_vault/key_vault_v1i1.h
+++ STMicroelectronics/STM32L4_L4+/packages/key_vault/key_vault_v1i1.h
@@ -2,8 +2,8 @@
 // Licensed under the MIT license.
 // See LICENSE file in the project root for full license information.
 
-#ifndef CIPHER_V1I1_H
-#define CIPHER_V1I1_H
+#ifndef KEY_VAULT_V1I1_H
+#define KEY_VAULT_V1I1_H
 
 #include "az_ulib_result.h"
 #include "azure/az_core.h"
@@ -16,15 +16,12 @@ extern "C"
 #include <stdint.h>
 #endif
 
-  az_result cipher_v1i1_create(void);
-  az_result cipher_v1i1_destroy(void);
+  az_result key_vault_v1i1_encrypt(uint32_t algorithm, az_span src, az_span* dest);
 
-  az_result cipher_v1i1_encrypt(uint32_t algorithm, az_span src, az_span* dest);
-
-  az_result cipher_v1i1_decrypt(az_span src, az_span* dest);
+  az_result key_vault_v1i1_decrypt(az_span src, az_span* dest);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* CIPHER_V1I1_H */
+#endif /* KEY_VAULT_V1I1_H */
