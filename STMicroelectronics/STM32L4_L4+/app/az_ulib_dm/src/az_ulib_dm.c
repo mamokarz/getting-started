6a144459da481a8b342427084ff97b7a3c212be5
Marcos Perez Mokarzel <mamokarz@microsoft.com>
2021-05-04 09:28:01 -0700

    Key_vault package.

--- STMicroelectronics/STM32L4_L4+/app/az_ulib_dm/src/az_ulib_dm.c
+++ STMicroelectronics/STM32L4_L4+/app/az_ulib_dm/src/az_ulib_dm.c
@@ -4,10 +4,15 @@
 
 #include "az_ulib_result.h"
 #include "az_ulib_dm_api.h"
+#include "az_ulib_ipc_api.h"
+#include "az_ulib_ipc_interface.h"
 #include "_az_ulib_dm_interface.h"
+#include "az_ulib_pal_os_api.h"
+#include "az_ulib_port.h"
 #include "azure/az_core.h"
 #include "cipher_v1i1.h"
 #include "cipher_v2i1.h"
+#include "dm_1_model.h"
 #include "sprinkler_v1i1.h"
 #include <azure/core/internal/az_precondition_internal.h>
 #include <string.h>
@@ -19,12 +24,52 @@
  */
 static az_ulib_dm* volatile _az_dm_cb = NULL;
 
+typedef az_result (*publish_interface)(const az_ulib_ipc_vtable* const vtable);
+typedef az_result (*unpublish_interface)(const az_ulib_ipc_vtable* const vtable);
+
+static _az_ulib_dm_package* get_first_package_free(void)
+{
+  for(int i = 0; i < AZ_ULIB_CONFIG_MAX_DM_PACKAGES; i++)
+  {
+    if(_az_dm_cb->_internal.package_list[i].address == NULL)
+    {
+      return &_az_dm_cb->_internal.package_list[i];
+    }
+  }
+  return NULL;
+}
+
+static _az_ulib_dm_package* get_package(az_span name)
+{
+  for(int i = 0; i < AZ_ULIB_CONFIG_MAX_DM_PACKAGES; i++)
+  {
+    if(_az_dm_cb->_internal.package_list[i].address != NULL)
+    {
+      if(az_span_is_content_equal(_az_dm_cb->_internal.package_list[i].name, name))
+      {
+        return &_az_dm_cb->_internal.package_list[i];
+      }
+    }
+  }
+  return NULL;
+}
+
 AZ_NODISCARD az_result az_ulib_dm_init(az_ulib_dm* dm_handle)
 {
   _az_PRECONDITION_IS_NULL(_az_dm_cb);
   _az_PRECONDITION_NOT_NULL(dm_handle);
 
   _az_dm_cb = dm_handle;
+
+  az_pal_os_lock_init(&(_az_dm_cb->_internal.lock));
+
+  for(int i = 0; i < AZ_ULIB_CONFIG_MAX_DM_PACKAGES; i++)
+  {
+    _az_ulib_dm_package* package = &(_az_dm_cb->_internal.package_list[i]);
+    package->address = NULL;
+    package->name = az_span_create(package->name_buf, AZ_ULIB_CONFIG_MAX_DM_PACKAGE_NAME);
+    az_span_fill(package->name, '\0');
+  }
 
   return _az_ulib_dm_interface_publish();
 }
@@ -33,10 +78,13 @@ AZ_NODISCARD az_result az_ulib_dm_deinit(void)
 {
   _az_PRECONDITION_NOT_NULL(_az_dm_cb);
 
+  az_pal_os_lock_deinit(&(_az_dm_cb->_internal.lock));
+  _az_dm_cb = NULL;
+
   return _az_ulib_dm_interface_unpublish();
 }
 
-AZ_NODISCARD az_result az_ulib_dm_install(az_span package_name)
+static az_result install_built_in(az_span package_name)
 {
   _az_PRECONDITION_NOT_NULL(_az_dm_cb);
   az_result result;
@@ -61,27 +109,134 @@ AZ_NODISCARD az_result az_ulib_dm_install(az_span package_name)
   return result;
 }
 
-AZ_NODISCARD az_result az_ulib_dm_uninstall(az_span package_name)
+static az_result install_in_memory(void* base_address, az_span package_name)
 {
   _az_PRECONDITION_NOT_NULL(_az_dm_cb);
   az_result result;
 
-  if(az_span_is_content_equal(package_name, AZ_SPAN_FROM_STR("cipher_v1i1")))
+  if(get_package(package_name) != NULL)
   {
-    result = cipher_v1i1_destroy();
-  }
-  else if(az_span_is_content_equal(package_name, AZ_SPAN_FROM_STR("cipher_v2i1")))
-  {
-    result = cipher_v2i1_destroy();
-  }
-  else if(az_span_is_content_equal(package_name, AZ_SPAN_FROM_STR("sprinkler_v1i1")))
-  {
-    result = sprinkler_v1i1_destroy();
+    result = AZ_ERROR_ULIB_ELEMENT_DUPLICATE;
   }
   else
   {
-    result = AZ_ERROR_ITEM_NOT_FOUND;
+    uint32_t publish_position = *((uint32_t*)base_address + AZ_ULIB_DM_PACKAGE_PUBLISH);
+    publish_interface publish = (publish_interface)((uint8_t*)base_address + publish_position + 
+        (AZ_ULIB_DM_PACKAGE_PUBLISH << 2));
+    const az_ulib_ipc_vtable* vtable = az_ulib_ipc_get_vtable();
+    if((result = publish(vtable)) == AZ_OK)
+    {
+      _az_ulib_dm_package* package = get_first_package_free();
+      if(package == NULL)
+      {
+        result = AZ_ERROR_NOT_ENOUGH_SPACE;
+      }
+      else
+      {
+        az_span_copy(package->name, package_name);
+        package->name = az_span_create(az_span_ptr(package->name), az_span_size(package_name));
+        package->address = base_address;
+        result = AZ_OK;
+      }
+    }
   }
+
+  return result;
+}
+
+static az_result install_from_blob(void* base_address, az_span package_name)
+{
+  _az_PRECONDITION_NOT_NULL(_az_dm_cb);
+  (void)base_address;
+  (void)package_name;
+
+  /* TODO: implement intall from Blobs here. */
+
+  return AZ_ERROR_NOT_IMPLEMENTED;
+}
+
+static az_result install_from_cli(void* base_address, az_span package_name)
+{
+  _az_PRECONDITION_NOT_NULL(_az_dm_cb);
+  (void)base_address;
+  (void)package_name;
+
+  /* TODO: implement intall from cli here. */
+
+  return AZ_ERROR_NOT_IMPLEMENTED;
+}
+
+AZ_NODISCARD az_result az_ulib_dm_install(dm_1_source_type source_type, void* base_address, az_span package_name)
+{
+  _az_PRECONDITION_NOT_NULL(_az_dm_cb);
+  az_result result;
+
+  az_pal_os_lock_acquire(&(_az_dm_cb->_internal.lock));
+  {
+    switch(source_type)
+    {
+      case DM_1_SOURCE_TYPE_IN_MEMORY:
+        result = install_in_memory(base_address, package_name);
+        break;
+      case DM_1_SOURCE_TYPE_BLOB:
+        result = install_from_blob(base_address, package_name);
+        break;
+      case DM_1_SOURCE_TYPE_CLI:
+        result = install_from_cli(base_address, package_name);
+        break;
+      case DM_1_SOURCE_TYPE_BUILT_IN:
+        result = install_built_in(package_name);
+        break;
+      default:
+        result = AZ_ERROR_UNEXPECTED_CHAR;
+        break;
+    }
+  }
+  az_pal_os_lock_release(&(_az_dm_cb->_internal.lock));
+
+  return result;
+}
+
+AZ_NODISCARD az_result az_ulib_dm_uninstall(az_span package_name)
+{
+  _az_PRECONDITION_NOT_NULL(_az_dm_cb);
+  az_result result;
+  _az_ulib_dm_package* package;
+
+  az_pal_os_lock_acquire(&(_az_dm_cb->_internal.lock));
+  {
+    if(az_span_is_content_equal(package_name, AZ_SPAN_FROM_STR("cipher_v1i1")))
+    {
+      result = cipher_v1i1_destroy();
+    }
+    else if(az_span_is_content_equal(package_name, AZ_SPAN_FROM_STR("cipher_v2i1")))
+    {
+      result = cipher_v2i1_destroy();
+    }
+    else if(az_span_is_content_equal(package_name, AZ_SPAN_FROM_STR("sprinkler_v1i1")))
+    {
+      result = sprinkler_v1i1_destroy();
+    }
+    else if((package = get_package(package_name)) != NULL)
+    {
+      uint32_t unpublish_position = *((uint32_t*)package->address + AZ_ULIB_DM_PACKAGE_UNPUBLISH);
+      unpublish_interface unpublish = 
+          (unpublish_interface)((uint8_t*)package->address + unpublish_position + 
+              (AZ_ULIB_DM_PACKAGE_UNPUBLISH << 2));
+      const az_ulib_ipc_vtable* vtable = az_ulib_ipc_get_vtable();
+      if((result = unpublish(vtable)) == AZ_OK)
+      {
+        package->address = NULL;
+        package->name = az_span_create(package->name_buf, AZ_ULIB_CONFIG_MAX_DM_PACKAGE_NAME);
+        az_span_fill(package->name, '\0');
+      }
+    }
+    else
+    {
+      result = AZ_ERROR_ITEM_NOT_FOUND;
+    }
+  }
+  az_pal_os_lock_release(&(_az_dm_cb->_internal.lock));
 
   return result;
 }
