c3a250fc01630b09245d6c783ac954af08e0e8ef
Cindy Deng <cindydeng@microsoft.com>
2021-07-27 11:24:14 -0700

    add get POC

--- STMicroelectronics/STM32L4_L4+/app/az_ulib_dm/inc/az_ulib_registry_api.h
+++ STMicroelectronics/STM32L4_L4+/app/az_ulib_dm/inc/az_ulib_registry_api.h
@@ -15,7 +15,6 @@
 
 #include "az_ulib_result.h"
 #include "azure/az_core.h"
-#include "registry_1_model.h"
 
 #ifndef __cplusplus
 #include <stdint.h>
@@ -27,27 +26,97 @@
 
 #define AZ_ULIB_CONFIG_MAX_REGISTRY_ENTRIES 10
 
-// any needed internal data structure
-typedef struct
+typedef enum
 {
-  az_span key;
-  az_span value;
-} _az_ulib_registry_entry;
+  REGISTRY_USED = 0x00,
+  REGISTRY_FREE = 0xFF
+} az_registry_entry_type;
 
 /**
- * @brief Registry Editor handle.
+ * @brief   Structure for the registry control block
+ *
+ * Every entry into the registry of the device will have a registry control block that contains
+ * relevant information to restore the state of the registry after device power cycle.
+ *
  */
-typedef struct az_ulib_registry_tag
-{
-  struct
-  {
-    _az_ulib_registry_entry registry_entry[AZ_ULIB_CONFIG_MAX_REGISTRY_ENTRIES];
-    uint8_t num_entries;
-  } _internal;
-} az_ulib_registry;
 
-AZ_NODISCARD az_result az_ulib_registry_get_value(az_span key, az_span value);
+typedef struct
+{
+  /** The enum that declares whether a registry slot is occupied or not.*/
+  az_registry_entry_type used_flag;
+
+  /** The az_span that contains pointer to a location in flash storing the key char string and the
+   * string's size.*/
+  az_span key;
+
+  /** The az_span that contains pointer to a location in flash storing the valye char string and the
+   * string's size.*/
+  az_span value;
+} az_registry_block;
+
+/**
+ * @brief   This function gets the az_span value associated with the given az_span key from the
+ * registry.
+ *
+ * This function goes through the registry comparing keys until it finds one that matches with the
+ * input.
+ *
+ * @param[in]   key                  The az_span key to look for within the registry.
+ * @param[in]   value                The az_span value corresponding to the input key
+ *
+ * @pre         \p key                            shall not be `NULL`.
+ * @pre         \p sizeof(key)                    shall greater than zero.
+ *
+ * @return The #az_result with the result of the registry operations.
+ *      @retval #AZ_OK                        If retrieving a value from the registry was
+ *                                            successful
+ *      @retval #AZ_ERROR_ITEM_NOT_FOUND      If there are no values that correspond to the
+ *                                            given key within the registry. *
+ *      @retval #AZ_ERROR_ARG                 If the parameters passed into`az_ulib_registry_get`
+ *                                            violate requirements for this function.
+ */
+AZ_NODISCARD az_result az_ulib_registry_get(az_span key, az_span* value);
+
+/**
+ * @brief   This function adds an az_span key and an az_span value into the device registry.
+ *
+ * This function goes through the registry ensuring there are no duplicate elements before
+ * adding a new key value pair to the registry.
+ *
+ * @param[in]   key                  The az_span key to add to the registry.
+ * @param[in]   value                The az_span value to add to the registry.
+ *
+ * @pre         \p key                            shall not be `NULL`.
+ * @pre         \p sizeof(key)                    shall greater than zero.
+ * @pre         \p value                          shall not be `NULL`.
+ * @pre         \p sizeof(value)                  shall greater than zero.
+ *
+ * @return The #az_result with the result of the registry operations.
+ *      @retval #AZ_OK                            If adding a value to the registry was successful
+ *      @retval #AZ_ERROR_ULIB_ELEMENT_DUPLICATE  If there is a key within the registry that is the
+ *                                                same as the new key.
+ *      @retval #AZ_ERROR_ULIB_SYSTEM             If the `az_ulib_registry_add` operation failed on
+ *                                                the system level.
+ *      @retval #AZ_ERROR_ULIB_BUSY               If the resources necesary for the
+ *                                                `az_ulib_registry_add` operation are busy.
+ *      @retval #AZ_ERROR_ARG                     If the parameters passed into
+ *                                                `az_ulib_registry_add` violate requirements for
+ *                                                this function.
+ */
 AZ_NODISCARD az_result az_ulib_registry_add(az_span key, az_span value);
+
+AZ_NODISCARD az_result az_ulib_registry_delete(az_span key);
+
+/**
+ * @brief   This function initializes the device registry.
+ *
+ * This function restores the state of the registry after reboot and repopulates the registry editor
+ * with registry control blocks.
+ *
+ * @return The #az_result with the result of the registry operations.
+ *      @retval #AZ_OK                            If initializing the registry was successful. 
+ */
+AZ_NODISCARD az_result az_ulib_registry_init();
 
 #include "azure/core/_az_cfg_suffix.h"
 
--- STMicroelectronics/STM32L4_L4+/app/az_ulib_dm/src/az_ulib_registry.c
+++ STMicroelectronics/STM32L4_L4+/app/az_ulib_dm/src/az_ulib_registry.c
@@ -3,23 +3,191 @@
 // See LICENSE file in the project root for full license information.
 
 #include "_az_ulib_registry_interface.h"
-#include "az_ulib_registry_api.h"
 #include "az_ulib_ipc_api.h"
 #include "az_ulib_ipc_interface.h"
+#include "az_ulib_registry_api.h"
 #include "az_ulib_result.h"
-#include "azure/az_core.h"
-#include "registry_1_model.h"
+#include "stm32l475_flash_driver.h"
 #include <azure/core/internal/az_precondition_internal.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 
-// reference location from linker to save registry info
+#define MAX_AZ_ULIB_REGISTRY_ENTRIES 10
 
-// static functions
+static az_registry_block registry_editor[MAX_AZ_ULIB_REGISTRY_ENTRIES];
 
-// verify_space in registry
-// get all keys
-// store num entries
+extern uint32_t __REGISTRYINFO;
+extern uint32_t __REGISTRY;
 
-AZ_NODISCARD az_result az_ulib_registry_get_value(az_span key, az_span value);
+static az_result result_from_hal_status(HAL_StatusTypeDef status)
+{
+  switch (status)
+  {
+    case HAL_OK:
+      return AZ_OK;
+    case HAL_ERROR:
+      return AZ_ERROR_ULIB_SYSTEM;
+    case HAL_BUSY:
+      return AZ_ERROR_ULIB_BUSY;
+    case HAL_TIMEOUT:
+      return AZ_ERROR_ULIB_SYSTEM; // should be AZ_ERROR_ULIB_TIME_OUT
+    default:
+      return AZ_ERROR_ULIB_SYSTEM;
+  }
+}
 
-AZ_NODISCARD az_result az_ulib_registry_add(az_span key, az_span value);
+static HAL_StatusTypeDef store_registry()
+{
+  HAL_StatusTypeDef hal_status;
+  int32_t size = sizeof(az_registry_block) * MAX_AZ_ULIB_REGISTRY_ENTRIES;
+
+  // erase registry
+  if ((hal_status = internal_flash_erase((uint8_t*)(&__REGISTRYINFO), size)) != HAL_OK)
+  {
+    return hal_status;
+  }
+
+  if ((hal_status = internal_flash_write((uint8_t*)(&__REGISTRYINFO),
+           (uint8_t*)&registry_editor,
+           sizeof(az_registry_block) * MAX_AZ_ULIB_REGISTRY_ENTRIES))
+      != HAL_OK)
+  {
+    return hal_status;
+  }
+
+  return hal_status;
+}
+
+AZ_NODISCARD az_result az_ulib_registry_init()
+{
+  // read registry_editor from flash
+  memcpy(&registry_editor,
+      (uint8_t*)(&__REGISTRYINFO),
+      sizeof(az_registry_block) * MAX_AZ_ULIB_REGISTRY_ENTRIES);
+
+  return AZ_OK;
+}
+
+// Find pointer to the next available space in flash to store registry key/value
+// Will always be called after init
+static uint8_t* flash_buf_get()
+{
+  az_registry_block last_block;
+
+  // empty registry
+  if (registry_editor[0].used_flag == REGISTRY_FREE)
+  {
+    return (uint8_t*)(&__REGISTRY);
+  }
+
+  // The beginning of buffer space
+  uint8_t* max_address = (uint8_t*)(&__REGISTRY);
+
+  // registry with content
+  for (int i = 0; i < MAX_AZ_ULIB_REGISTRY_ENTRIES; i++)
+  {
+    if (registry_editor[i].used_flag == REGISTRY_USED)
+    {
+      if (az_span_ptr(registry_editor[i].value) > max_address)
+      {
+        max_address = az_span_ptr(registry_editor[i].value);
+        last_block  = registry_editor[i];
+      }
+    }
+  }
+
+  // last_block should have reference to the last block in available memory
+  uint8_t* new_block_ptr
+      = az_span_ptr(last_block.value) + ((((az_span_size(last_block.value) - 1) >> 3) + 1) << 3);
+  return new_block_ptr;
+}
+
+AZ_NODISCARD az_result az_ulib_registry_get(az_span key, az_span* value)
+{
+  // registry with content
+  for (int i = 0; i < MAX_AZ_ULIB_REGISTRY_ENTRIES; i++)
+  {
+    if (registry_editor[i].used_flag == REGISTRY_USED)
+    {
+      if (az_span_is_content_equal(key, registry_editor[i].key))
+      {
+        *value = registry_editor[i].value;
+        return AZ_OK;
+      }
+    }
+    else
+    {
+      return AZ_ERROR_ITEM_NOT_FOUND;
+    }
+  }
+
+  // item not found in registry
+  return AZ_ERROR_ITEM_NOT_FOUND;
+}
+
+AZ_NODISCARD az_result az_ulib_registry_add(az_span new_key, az_span new_value)
+{
+  int32_t free_block_index;
+  HAL_StatusTypeDef hal_status;
+  az_result result;
+
+  // validate input parameters 
+
+  // validate for duplicates before adding new entry
+  result = az_ulib_registry_get(new_key, &new_value);
+
+  if (result == AZ_OK)
+  {
+    return AZ_ERROR_ULIB_ELEMENT_DUPLICATE;
+  }
+  if (result != AZ_ERROR_ITEM_NOT_FOUND)
+  {
+    return AZ_ERROR_ULIB_SYSTEM;
+  }
+
+  // look for next available spot in list
+  for (int i = 0; i < MAX_AZ_ULIB_REGISTRY_ENTRIES; i++)
+  {
+    if (registry_editor[i].used_flag == REGISTRY_FREE)
+    {
+      free_block_index = i;
+      break;
+    }
+  }
+
+  // find destination in flash buffer
+  uint8_t* key_dest_ptr = flash_buf_get();
+  uint8_t* val_dest_ptr = key_dest_ptr
+      + ((((az_span_size(new_key) - 1) >> 3) + 1) << 3); // round to the nearest word address
+
+  // set free block information
+  registry_editor[free_block_index].used_flag = REGISTRY_USED;
+  registry_editor[free_block_index].key       = az_span_create(key_dest_ptr, az_span_size(new_key));
+  registry_editor[free_block_index].value = az_span_create(val_dest_ptr, az_span_size(new_value));
+
+  // write key to flash
+  if ((hal_status = internal_flash_write(key_dest_ptr, az_span_ptr(new_key), az_span_size(new_key)))
+      != HAL_OK)
+  {
+    return result_from_hal_status(hal_status);
+  }
+
+  // write value to flash
+  if ((hal_status
+          = internal_flash_write(val_dest_ptr, az_span_ptr(new_value), az_span_size(new_value)))
+      != HAL_OK)
+  {
+    return result_from_hal_status(hal_status);
+  }
+
+  // store registry state to flash
+  hal_status = store_registry();
+  if (hal_status != HAL_OK)
+  {
+    return result_from_hal_status(hal_status);
+  }
+
+  return AZ_OK;
+}
