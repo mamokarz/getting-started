116524cab2b7c6c45cd7891485a890becfa52fe9
Marcos Perez Mokarzel <mamokarz@users.noreply.github.com>
2021-08-23 09:31:32 -0700

    Add package relocation. (#20)

---
+++ STMicroelectronics/STM32L4_L4+/samples/sprinkler.1/sprinkler.c
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+#include "stm32l475xx.h"
+#include "stm32l4xx_hal.h"
+
+#include "az_ulib_result.h"
+#include "sprinkler.h"
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+
+az_result sprinkler_v1i1_water_now(int32_t zone, int32_t timer)
+{
+  if ((timer != 0) || (zone != 0))
+  {
+    return AZ_ERROR_NOT_SUPPORTED;
+  }
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
+
+  return AZ_OK;
+}
+
+az_result sprinkler_v1i1_stop(int32_t zone)
+{
+  if (zone != 0)
+  {
+    return AZ_ERROR_NOT_SUPPORTED;
+  }
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
+
+  return AZ_OK;
+}
+
+az_result sprinkler_v1i1_end(void) { return sprinkler_v1i1_stop(0); }
