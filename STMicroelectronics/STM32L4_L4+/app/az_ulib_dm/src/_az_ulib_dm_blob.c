4ad9b1a64c9a3bd1fbcc9619dbd398cc82b79bf7
Spencer McDonough <shmcdono@usc.edu>
2021-09-16 08:58:01 -0700

    rebase azure-rtos/getting-started/master - resolve merge conflicts (5)

--- STMicroelectronics/STM32L4_L4+/app/az_ulib_dm/src/_az_ulib_dm_blob.c
+++ STMicroelectronics/STM32L4_L4+/app/az_ulib_dm/src/_az_ulib_dm_blob.c
@@ -6,30 +6,30 @@
 #include "az_ulib_result.h"
 #include "azure/az_core.h"
 #include "nx_api.h"
-#include "nxd_dns.h"
 #include "nx_web_http_client.h"
-#include "stm_networking.h"
 #include "nx_wifi.h"
-#include "wifi.h"
+#include "nxd_dns.h"
 #include "stm32l475_flash_driver.h"
+#include "stm_networking.h"
+#include "wifi.h"
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 
-#define               USER_AGENT_NAME           "User-Agent: "
-#define               USER_AGENT_VALUE          "Azure RTOS Device (STM32)"
-#define               DCF_WAIT_TIME             600
+#define USER_AGENT_NAME  "User-Agent: "
+#define USER_AGENT_VALUE "Azure RTOS Device (STM32)"
+#define DCF_WAIT_TIME    600
 
 static int32_t slice_next_char(az_span span, int32_t start, uint8_t c, az_span* slice)
 {
   int32_t size = az_span_size(span);
   uint8_t* buf = az_span_ptr(span);
-  int32_t end = start;
-  while(end < size)
+  int32_t end  = start;
+  while (end < size)
   {
-    if(buf[end] == c)
+    if (buf[end] == c)
     {
-      if(slice != NULL)
+      if (slice != NULL)
       {
         *slice = az_span_slice(span, start, end);
       }
@@ -40,55 +40,51 @@ static int32_t slice_next_char(az_span span, int32_t start, uint8_t c, az_span* 
   return -1;
 }
 
-static az_result split_url(
-  az_span url, 
-  az_span* protocol, 
-  az_span* address,
-  az_span* resource, 
-  az_span* container, 
-  az_span* directories, 
-  az_span* file_name, 
-  az_span* sas_token)
+static az_result split_url(az_span url,
+    az_span* protocol,
+    az_span* address,
+    az_span* resource,
+    az_span* container,
+    az_span* directories,
+    az_span* file_name,
+    az_span* sas_token)
 {
   AZ_ULIB_TRY
   {
     int32_t next;
 
     /* Get protocol http:// or https:// */
-    AZ_ULIB_THROW_IF_ERROR(
-        ((next = az_span_find(url, AZ_SPAN_FROM_STR("://"))) != -1), AZ_ERROR_UNEXPECTED_CHAR);
-    if(protocol != NULL)
+    AZ_ULIB_THROW_IF_ERROR(((next = az_span_find(url, AZ_SPAN_FROM_STR("://"))) != -1), AZ_ERROR_UNEXPECTED_CHAR);
+    if (protocol != NULL)
     {
       *protocol = az_span_slice(url, 0, next);
     }
 
     /* Get address. */
-    AZ_ULIB_THROW_IF_ERROR(
-        ((next = slice_next_char(url, next + 3, '/', address)) != -1), AZ_ERROR_UNEXPECTED_CHAR);
+    AZ_ULIB_THROW_IF_ERROR(((next = slice_next_char(url, next + 3, '/', address)) != -1), AZ_ERROR_UNEXPECTED_CHAR);
 
     /* Get resource span. */
-    if(resource != NULL)
+    if (resource != NULL)
     {
       *resource = az_span_slice_to_end(url, next + 1);
     }
 
     /* Get container name. */
-    AZ_ULIB_THROW_IF_ERROR(
-        ((next = slice_next_char(url, next + 1, '/', container)) != -1), AZ_ERROR_UNEXPECTED_CHAR);
+    AZ_ULIB_THROW_IF_ERROR(((next = slice_next_char(url, next + 1, '/', container)) != -1), AZ_ERROR_UNEXPECTED_CHAR);
 
     // Get directories, if exist. */
     // TODO: add code to read directories here.
 
     // Get file_name. */
-    AZ_ULIB_THROW_IF_ERROR(
-        ((next = slice_next_char(url, next + 1, '?', file_name)) != -1), AZ_ERROR_UNEXPECTED_CHAR);
+    AZ_ULIB_THROW_IF_ERROR(((next = slice_next_char(url, next + 1, '?', file_name)) != -1), AZ_ERROR_UNEXPECTED_CHAR);
 
     // Get sas_token. */
-    if(sas_token != NULL) 
+    if (sas_token != NULL)
     {
       *sas_token = az_span_slice_to_end(url, next + 1);
     }
-  } AZ_ULIB_CATCH(...) 
+  }
+  AZ_ULIB_CATCH(...)
   {
     return AZ_ULIB_TRY_RESULT;
   }
@@ -103,15 +99,12 @@ static az_result get_ip_from_uri(az_span uri, NXD_ADDRESS* ip)
     /* Get the IPv4 for the URI using DNS. */
     char uri_str[50];
     az_span_to_str(uri_str, sizeof(uri_str), uri);
-    UINT status = nxd_dns_host_by_name_get(
-            &nx_dns_client,
-            (UCHAR*)uri_str, 
-            ip, 
-            NX_IP_PERIODIC_RATE, 
-            NX_IP_VERSION_V4);
+    UINT status = nxd_dns_host_by_name_get(&nx_dns_client, (UCHAR*)uri_str, ip, NX_IP_PERIODIC_RATE, NX_IP_VERSION_V4);
     AZ_ULIB_THROW_IF_ERROR((status == NX_SUCCESS), AZ_ERROR_ULIB_SYSTEM);
-
-  } AZ_ULIB_CATCH(...) {}
+  }
+  AZ_ULIB_CATCH(...)
+  {
+  }
 
   return AZ_ULIB_TRY_RESULT;
 }
@@ -121,9 +114,11 @@ AZ_NODISCARD az_result _az_ulib_dm_blob_get_package_name(az_span url, az_span* n
   AZ_ULIB_TRY
   {
     AZ_ULIB_THROW_IF_AZ_ERROR(split_url(url, NULL, NULL, NULL, NULL, NULL, name, NULL));
-    AZ_ULIB_THROW_IF_ERROR(
-        (slice_next_char(*name, 0, '.', name) != -1), AZ_ERROR_UNEXPECTED_CHAR);
-  } AZ_ULIB_CATCH(...) {}
+    AZ_ULIB_THROW_IF_ERROR((slice_next_char(*name, 0, '.', name) != -1), AZ_ERROR_UNEXPECTED_CHAR);
+  }
+  AZ_ULIB_CATCH(...)
+  {
+  }
 
   return AZ_ULIB_TRY_RESULT;
 }
@@ -137,31 +132,46 @@ AZ_NODISCARD az_result _az_ulib_dm_blob_get_size(az_span url, int32_t* size)
 
 static az_result result_from_nx_status(UINT nx_status)
 {
-  switch(nx_status)
+  switch (nx_status)
   {
-    case NX_SUCCESS:              return AZ_OK;
-    case NX_WEB_HTTP_POOL_ERROR:  return AZ_ERROR_NOT_ENOUGH_SPACE;
-    case NX_NO_PACKET:            return AZ_ERROR_NOT_ENOUGH_SPACE;
-    case NX_INVALID_PARAMETERS:   return AZ_ERROR_NOT_SUPPORTED;
-    case NX_OPTION_ERROR:         return AZ_ERROR_ARG;
-    case NX_PTR_ERROR:            return AZ_ERROR_ARG;
-    case NX_CALLER_ERROR:         return AZ_ERROR_ARG;
-    case NX_WEB_HTTP_NOT_READY:   return AZ_ERROR_ULIB_BUSY;
-    case NX_WAIT_ABORTED:         return AZ_ERROR_ULIB_SYSTEM;  //should be AZ_ERROR_ULIB_TIME_OUT
-                                                                // or AZ_ERROR_ULIB_ABORTED
-    default:                      return AZ_ERROR_ULIB_SYSTEM; 
+    case NX_SUCCESS:
+      return AZ_OK;
+    case NX_WEB_HTTP_POOL_ERROR:
+      return AZ_ERROR_NOT_ENOUGH_SPACE;
+    case NX_NO_PACKET:
+      return AZ_ERROR_NOT_ENOUGH_SPACE;
+    case NX_INVALID_PARAMETERS:
+      return AZ_ERROR_NOT_SUPPORTED;
+    case NX_OPTION_ERROR:
+      return AZ_ERROR_ARG;
+    case NX_PTR_ERROR:
+      return AZ_ERROR_ARG;
+    case NX_CALLER_ERROR:
+      return AZ_ERROR_ARG;
+    case NX_WEB_HTTP_NOT_READY:
+      return AZ_ERROR_ULIB_BUSY;
+    case NX_WAIT_ABORTED:
+      return AZ_ERROR_ULIB_SYSTEM; // should be AZ_ERROR_ULIB_TIME_OUT
+                                   // or AZ_ERROR_ULIB_ABORTED
+    default:
+      return AZ_ERROR_ULIB_SYSTEM;
   }
 }
 
 static az_result result_from_hal_status(HAL_StatusTypeDef status)
 {
-  switch(status)
+  switch (status)
   {
-    case HAL_OK:                  return AZ_OK;
-    case HAL_ERROR:               return AZ_ERROR_ULIB_SYSTEM;
-    case HAL_BUSY:                return AZ_ERROR_ULIB_BUSY;
-    case HAL_TIMEOUT:             return AZ_ERROR_ULIB_SYSTEM;  //should be AZ_ERROR_ULIB_TIME_OUT
-    default:                      return AZ_ERROR_ULIB_SYSTEM; 
+    case HAL_OK:
+      return AZ_OK;
+    case HAL_ERROR:
+      return AZ_ERROR_ULIB_SYSTEM;
+    case HAL_BUSY:
+      return AZ_ERROR_ULIB_BUSY;
+    case HAL_TIMEOUT:
+      return AZ_ERROR_ULIB_SYSTEM; // should be AZ_ERROR_ULIB_TIME_OUT
+    default:
+      return AZ_ERROR_ULIB_SYSTEM;
   }
 }
 
@@ -175,92 +185,87 @@ static az_result copy_blob_to_flash(NXD_ADDRESS* ip, CHAR* resource, CHAR* host,
   HAL_StatusTypeDef hal_status;
 
   // create http blob client
-  if((nx_status = nx_web_http_client_create(&http_client, "HTTP Client", 
-                              &nx_ip, &nx_pool, 1536)) == NX_SUCCESS)
+  if ((nx_status = nx_web_http_client_create(&http_client, "HTTP Client", &nx_ip, &nx_pool, 1536)) == NX_SUCCESS)
   {
     // connect to server
-    if((nx_status = nx_web_http_client_connect(&http_client, ip, 
-                              NX_WEB_HTTP_SERVER_PORT, DCF_WAIT_TIME)) == NX_SUCCESS)
+    if ((nx_status = nx_web_http_client_connect(&http_client, ip, NX_WEB_HTTP_SERVER_PORT, DCF_WAIT_TIME)) ==
+        NX_SUCCESS)
     {
       // initialize get request
-      if((nx_status = nx_web_http_client_request_initialize(&http_client,
-                                NX_WEB_HTTP_METHOD_GET, /* GET, PUT, DELETE, POST, HEAD */
-                                resource, /* "resource" (usually includes auth headers)*/
-                                host, /* "host" */
-                                0, /* Used by PUT and POST */
-                                NX_FALSE, /* If true, input_size is ignored. */
-                                NX_NULL, /* "name" */
-                                NX_NULL, /* "password" */
-                                DCF_WAIT_TIME)) == NX_SUCCESS)
+      if ((nx_status = nx_web_http_client_request_initialize(&http_client,
+               NX_WEB_HTTP_METHOD_GET, /* GET, PUT, DELETE, POST, HEAD */
+               resource,               /* "resource" (usually includes auth headers)*/
+               host,                   /* "host" */
+               0,                      /* Used by PUT and POST */
+               NX_FALSE,               /* If true, input_size is ignored. */
+               NX_NULL,                /* "name" */
+               NX_NULL,                /* "password" */
+               DCF_WAIT_TIME)) == NX_SUCCESS)
       {
         // add user agent
-        if((nx_status = nx_web_http_client_request_header_add(&http_client, 
-                                  USER_AGENT_NAME, 
-                                  sizeof(USER_AGENT_NAME) - 1, 
-                                  USER_AGENT_VALUE, 
-                                  sizeof(USER_AGENT_VALUE) - 1, 
-                                  DCF_WAIT_TIME)) == NX_SUCCESS)
+        if ((nx_status = nx_web_http_client_request_header_add(&http_client,
+                 USER_AGENT_NAME,
+                 sizeof(USER_AGENT_NAME) - 1,
+                 USER_AGENT_VALUE,
+                 sizeof(USER_AGENT_VALUE) - 1,
+                 DCF_WAIT_TIME)) == NX_SUCCESS)
         {
           // send request
-          if((nx_status = nx_web_http_client_request_send(&http_client, 
-                                                          DCF_WAIT_TIME)) == NX_SUCCESS)
+          if ((nx_status = nx_web_http_client_request_send(&http_client, DCF_WAIT_TIME)) == NX_SUCCESS)
           {
             // grab first chunk
             nx_status = nx_web_http_client_response_body_get(&http_client, &packet_ptr, 500);
             if ((nx_status == NX_SUCCESS) || (nx_status == NX_WEB_HTTP_GET_DONE))
             {
               // save pointer to first chunk
-              az_span data = az_span_create(packet_ptr->nx_packet_prepend_ptr, 
-                                                  packet_ptr->nx_packet_length);
+              az_span data = az_span_create(packet_ptr->nx_packet_prepend_ptr, packet_ptr->nx_packet_length);
 
               // calculate destination pointer
               uint8_t* dest_ptr = (uint8_t*)(address);
 
               // grab package size and round up to nearest 2KB (0x0800)
               uint32_t package_size = (uint32_t)http_client.nx_web_http_client_total_receive_bytes;
-              if((package_size & 0x07FF) != 0x000) 
-              { 
-                package_size = (package_size & 0xFFFFF800) + 0x0800; 
+              if ((package_size & 0x07FF) != 0x000)
+              {
+                package_size = (package_size & 0xFFFFF800) + 0x0800;
               }
 
               // erase flash
               if ((hal_status = internal_flash_erase((UCHAR*)address, package_size)) == HAL_OK)
               {
                 // write first chunk to flash
-                if((hal_status = internal_flash_write(
-                                      dest_ptr, az_span_ptr(data), az_span_size(data))) == HAL_OK)
+                if ((hal_status = internal_flash_write(dest_ptr, az_span_ptr(data), az_span_size(data))) == HAL_OK)
                 {
                   // if there are more chunks to store, loop over them until done
-                  while(nx_status == NX_SUCCESS)
+                  while (nx_status == NX_SUCCESS)
                   {
                     // release packet_ptr from last nx_web_htt_client_response_body_get()
                     nx_status = nx_packet_release(packet_ptr);
 
                     // grab next chunk
                     nx_status = nx_web_http_client_response_body_get(&http_client, &packet_ptr, 500);
-                    if((nx_status == NX_SUCCESS) || (nx_status == NX_WEB_HTTP_GET_DONE))
+                    if ((nx_status == NX_SUCCESS) || (nx_status == NX_WEB_HTTP_GET_DONE))
                     {
                       // increase destination pointer by size of last chunk
                       dest_ptr += az_span_size(data);
 
                       // save pointer to chunk
-                      data = az_span_create(packet_ptr->nx_packet_prepend_ptr, 
-                                                    packet_ptr->nx_packet_length);
+                      data = az_span_create(packet_ptr->nx_packet_prepend_ptr, packet_ptr->nx_packet_length);
 
                       // call store to flash
-                      if((hal_status = internal_flash_write(
-                                            dest_ptr, az_span_ptr(data), az_span_size(data))) != HAL_OK)
+                      if ((hal_status = internal_flash_write(dest_ptr, az_span_ptr(data), az_span_size(data))) !=
+                          HAL_OK)
                       {
                         break;
                       }
                     }
                   }
                 }
-                
-                if(nx_status == NX_WEB_HTTP_GET_DONE)
+
+                if (nx_status == NX_WEB_HTTP_GET_DONE)
                 {
                   nx_status = NX_SUCCESS;
-                }            
+                }
               }
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -281,20 +286,24 @@ static az_result copy_blob_to_flash(NXD_ADDRESS* ip, CHAR* resource, CHAR* host,
 >>>>>>> bc2bf24 (Address PR comments.)
 =======
             }
+<<<<<<< HEAD
           }        
 >>>>>>> 5a23efd (fix nx_packet_release() decision tree, add indentwidth to clang-format)
+=======
+          }
+>>>>>>> 6ae3692 (MSFT format with clang-format)
         }
       }
     }
-    
+
     nx_status = nx_web_http_client_delete(&http_client);
   }
 
-  if(nx_status != NX_SUCCESS)
+  if (nx_status != NX_SUCCESS)
   {
     return result_from_nx_status(nx_status);
   }
-  if(hal_status != HAL_OK)
+  if (hal_status != HAL_OK)
   {
     return result_from_hal_status(hal_status);
   }
@@ -308,18 +317,20 @@ AZ_NODISCARD az_result _az_ulib_dm_blob_download(void* address, az_span url)
     az_span uri = AZ_SPAN_EMPTY;
     NXD_ADDRESS ip;
     az_span resource;
-    
+
     AZ_ULIB_THROW_IF_AZ_ERROR(split_url(url, NULL, &uri, &resource, NULL, NULL, NULL, NULL));
 
-    AZ_ULIB_THROW_IF_AZ_ERROR(get_ip_from_uri(uri, &ip)); 
-    
+    AZ_ULIB_THROW_IF_AZ_ERROR(get_ip_from_uri(uri, &ip));
+
     char host[50];
     az_span_to_str(host, sizeof(host), uri);
     char resource_str[200];
     az_span_to_str(resource_str, sizeof(resource_str), resource);
     AZ_ULIB_THROW_IF_AZ_ERROR(copy_blob_to_flash(&ip, resource_str, host, address));
-
-  } AZ_ULIB_CATCH(...) {}
+  }
+  AZ_ULIB_CATCH(...)
+  {
+  }
 
   return AZ_ULIB_TRY_RESULT;
 }
